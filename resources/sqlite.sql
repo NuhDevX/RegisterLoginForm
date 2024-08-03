81177f7ce59d1f319ca99fef3a658b4a6797b1be
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-08-03 20:22:04 +0700

    Update sqlite.sql

--- resources/sqlite.sql
+++ resources/sqlite.sql
@@ -1,7 +1,17 @@
--- Create the users table
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    registered_at INTEGER NOT NULL
-);
+{
+    "users.create_table": [
+        "CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT, registered_at INTEGER);"
+    ],
+    "users.select_user": [
+        "SELECT * FROM users WHERE username = :username;"
+    ],
+    "users.insert_user": [
+        "INSERT INTO users (username, password, registered_at) VALUES (:username, :password, :registered_at);"
+    ],
+    "users.delete_user": [
+        "DELETE FROM users WHERE username = :username;"
+    ],
+    "users.select_all_users": [
+        "SELECT * FROM users;"
+    ]
+}
