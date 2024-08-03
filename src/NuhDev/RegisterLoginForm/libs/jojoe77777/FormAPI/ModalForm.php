b6ed759ee00e9f723d7b1516d56db3dfe1190126
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2024-08-03 11:44:29 +0700

    Update ModalForm.php

--- src/NuhDev/RegisterLoginForm/libs/jojoe77777/FormAPI/ModalForm.php
+++ src/NuhDev/RegisterLoginForm/libs/jojoe77777/FormAPI/ModalForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace NuhDev\RegisterLoginForm\jojoe77777\FormAPI;
+namespace NuhDev\RegisterLoginForm\libs\jojoe77777\FormAPI;
 
 class ModalForm extends Form {
 
